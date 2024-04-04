a363e079eddb53f12239694da3d91dc98746d702
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-04-04 07:29:30 +0200

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,1 @@
-import { configureStore } from '@reduxjs/toolkit'
-import accountReducer from "./slices/accountSlice"
 
-export const store = configureStore({
-  reducer: {
-    account: accountReducer
-  },
-})
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
